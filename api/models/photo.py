740f67de42c51e0cee0b5ebac50be633aef304ce
E-20-8-1-N <82087610+E-20-8-1-N@users.noreply.github.com>
2025-01-27 18:14:41 +0800

    Update photo.py

--- api/models/photo.py
+++ api/models/photo.py
@@ -406,7 +406,7 @@ class Photo(models.Model):
             ):
                 createAnimatedThumbnail(
                     inputPath=self.main_file.path,
-                    outputHeight=500,
+                    outputHeight=1080,
                     outputPath="square_thumbnails",
                     hash=self.image_hash,
                     fileType=".mp4",
